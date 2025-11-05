c21b66376b27f6a94ce50ef89c46639aef20cb39
minsik0 <qwew1212@naver.com>
2025-11-05 21:51:25 +0900

    [feat] Hub Enntity 추가

--- com.hub-service/src/main/java/com/hub_service/domain/model/Hub.java
+++ com.hub-service/src/main/java/com/hub_service/domain/model/Hub.java
@@ -1,4 +1,45 @@
 package com.hub_service.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "hubs")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hub {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private double latitude;
+
+    @Column(nullable = false)
+    private double longitude;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
