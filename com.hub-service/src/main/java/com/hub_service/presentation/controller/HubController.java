08c14e893016468a9bd2dbd6816ca1d480a35e81
minsik0 <qwew1212@naver.com>
2025-11-05 22:40:27 +0900

    [feat] HubController 구현

--- com.hub-service/src/main/java/com/hub_service/presentation/controller/HubController.java
+++ com.hub-service/src/main/java/com/hub_service/presentation/controller/HubController.java
@@ -2,14 +2,14 @@ package com.hub_service.presentation.controller;
 
 
 import com.hub_service.application.HubServiceV1;
+import com.hub_service.presentation.dto.request.HubRequestDto;
 import com.hub_service.presentation.dto.response.HubResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,9 +20,28 @@ public class HubController {
 
     @GetMapping("/hubs")
     public ResponseEntity<List<HubResponseDto>> getHubs(){
-        List<HubResponseDto> hubs = hubServiceV1.getAllHubs();
-        return ResponseEntity.ok(hubs);
+        return ResponseEntity.ok(hubServiceV1.getAllHubs());
     }
 
+    @GetMapping("/hubs/{hubId}")
+    public ResponseEntity<HubResponseDto> getHub(@PathVariable UUID id){
+        return ResponseEntity.ok(HubServiceV1.getHubById(id));
+    }
+
+    @PostMapping("/hubs")
+    public ResponseEntity<HubResponseDto> createHub(@RequestBody HubRequestDto requestDto){
+        return ResponseEntity.ok(HubServiceV1.createHub(requestDto));
+    }
+
+    @PatchMapping("/hubs/{hubId}")
+    public ResponseEntity<HubResponseDto> updateHub(@PathVariable UUID id, @RequestBody HubRequestDto requestDto){
+        return ResponseEntity.ok(HubServiceV1.updateHub(requestDto));
+    }
+
+    @DeleteMapping("/hubs/{hudId}")
+    public ResponseEntity<String> deleteHub(@PathVariable UUID id){
+        hubServiceV1.deleteHub(id);
+        return ResponseEntity.ok("허브가 삭제되었습니다.");
+    }
 
 }
