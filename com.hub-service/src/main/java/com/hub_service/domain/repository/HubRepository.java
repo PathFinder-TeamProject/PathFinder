e20e18f86072ebb5a19c2b3fa59bf027de6842ec
minsik0 <qwew1212@naver.com>
2025-11-06 00:57:06 +0900

    [feat] HubServiceV1 구현

--- com.hub-service/src/main/java/com/hub_service/domain/repository/HubRepository.java
+++ com.hub-service/src/main/java/com/hub_service/domain/repository/HubRepository.java
@@ -2,8 +2,10 @@ package com.hub_service.domain.repository;
 
 import com.hub_service.domain.model.Hub;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface HubRepository extends JpaRepository<Hub, UUID> {
+    UUID id(UUID id);
 }
