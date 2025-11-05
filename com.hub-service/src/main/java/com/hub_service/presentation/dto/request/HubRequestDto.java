603f477043b32b5e5e4507edcc2edaff0c85aebf
minsik0 <qwew1212@naver.com>
2025-11-05 21:51:59 +0900

    [feat] HubDto 추가

--- com.hub-service/src/main/java/com/hub_service/presentation/dto/request/HubRequestDto.java
+++ com.hub-service/src/main/java/com/hub_service/presentation/dto/request/HubRequestDto.java
@@ -1,4 +1,11 @@
 package com.hub_service.presentation.dto.request;
 
+import lombok.Data;
+
+@Data
 public class HubRequestDto {
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
 }
--- com.hub-service/src/main/java/com/hub_service/presentation/dto/response/HubResponseDto.java
+++ com.hub-service/src/main/java/com/hub_service/presentation/dto/response/HubResponseDto.java
@@ -5,14 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class HubResponseDto {
-    private String hubId;
-    private String hubName;
-    private String hubAddress;
+    private UUID id;
+    private String name;
+    private String address;
     private double latitude;
     private double longitude;
 }
