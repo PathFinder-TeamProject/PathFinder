4bae16ca41d0a39b4515a40c3e981d704b48b80c
minsik0 <qwew1212@naver.com>
2025-11-09 02:41:01 +0900

    [feat] 검색기능, 논리삭제 관련 컨트롤러 추가

--- com.hub-service/src/main/java/com/hub_service/presentation/controller/HubController.java
+++ com.hub-service/src/main/java/com/hub_service/presentation/controller/HubController.java
@@ -4,7 +4,12 @@ package com.hub_service.presentation.controller;
 import com.hub_service.application.HubServiceV1;
 import com.hub_service.presentation.dto.request.HubRequestDto;
 import com.hub_service.presentation.dto.response.HubResponseDto;
+import com.hub_service.presentation.dto.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,9 +44,25 @@ public class HubController {
     }
 
     @DeleteMapping("/hubs/{hudId}")
-    public ResponseEntity<String> deleteHub(@PathVariable UUID hudId){
-        hubServiceV1.deleteHub(hudId);
-        return ResponseEntity.ok("허브가 삭제되었습니다.");
+    public ResponseEntity<ResponseDto<String>> deleteHub(
+            @PathVariable UUID hudId,
+            @RequestParam(defaultValue = "master") String username
+    ){
+        hubServiceV1.deleteHub(hudId, username);
+        return ResponseEntity.ok(ResponseDto.success("허브 삭제 완료", "허브가 논리적으로 삭제되었습니다."));
+    }
+
+    @GetMapping("/hubs/search")
+    public ResponseEntity<ResponseDto<Page<HubResponseDto>>> searchHubs(
+            @RequestParam(required = false) String keyword,
+            @RequestParam(defaultValue = "createdAt") String sortBy,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        Pageable pageable =  PageRequest.of(page, size);
+        Page<HubResponseDto> result = hubServiceV1.searchHubs(keyword, sortBy, pageable);
+        return ResponseEntity.ok(ResponseDto.success("허브 검색 성공", result));
+
     }
 
 }
