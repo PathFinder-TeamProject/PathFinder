213293677bde420cf4d6b5bafda31733410c5282
raebagi <prh0208@naver.com>
2025-11-11 18:26:01 +0900

    feat[#33] : Product CRUD 구현 및 company의 안쓰는 임포트문 제거

--- company/src/main/java/com/pathfinder/company/infrastructure/config/CacheConfig.java
+++ company/src/main/java/com/pathfinder/company/infrastructure/config/CacheConfig.java
@@ -2,25 +2,30 @@ package com.pathfinder.company.infrastructure.config;
 
 import java.time.Duration;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 @EnableCaching
-public class cacheConfig {
+public class CacheConfig {
 
 	@Bean
-	public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
 		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-			.entryTtl(Duration.ofMinutes(10)) // TTL 10분
-			.disableCachingNullValues();
+			.serializeValuesWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(
+					new GenericJackson2JsonRedisSerializer() // ✅ JSON 직렬화로 교체
+				)
+			)
+			.entryTtl(Duration.ofMinutes(10)); // 캐시 TTL 10분 예시
 
-		return RedisCacheManager.builder(connectionFactory)
+		return RedisCacheManager.builder(redisConnectionFactory)
 			.cacheDefaults(config)
 			.build();
 	}
