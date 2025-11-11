fa9263d0e6cef41c735e9826dfdf6423215591f8
minsik0 <qwew1212@naver.com>
2025-11-11 15:07:52 +0900

    [feat] HubRouteRepositoryCustom 메서드 추가

--- com.hub-service/src/main/java/com/hub_service/domain/repository/HubRouteRepositoryCustom.java
+++ com.hub-service/src/main/java/com/hub_service/domain/repository/HubRouteRepositoryCustom.java
@@ -5,5 +5,5 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface HubRouteRepositoryCustom {
-    Page<HubRoute> searchRoute(String originName, Double minDistance, Double maxDistance, Pageable pageable);
+    Page<HubRoute> searchRoutes(String originName, String destinationName, Double minDistance, Double maxDistance, Pageable pageable);
 }
